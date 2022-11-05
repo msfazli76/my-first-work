6eb873645352c1c065e3bca772c9ac4ff0a206aa
msfazli76 <115671736+msfazli76@users.noreply.github.com>
2022-11-05 08:29:55 -0700

    Add files via upload

---
+++ p2222.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+main() {
+	int a,b,c,d,e,f;
+	//performing mathematical operations on two numbers//
+	 
+	cout<<"enter first number:";
+	cin>>a;
+	cout<<"enter second number:";
+	cin>>b;
+	cout<<"a+b=";
+	c=a+b;
+	cout<<c;
+	cout<<"\n";
+	cout<<"a-b=";
+	d=a-b;
+	cout<<d;
+	cout<<"\n";
+	cout<<"a*b=";
+	e=a*b;
+	cout<<e;
+	cout<<"\n";
+	cout<<"a%B=";
+	f=a%b;
+	cout<<f;
+}
