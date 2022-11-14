353bae5d5856cb32c3c24ff6c313d84e487a2333
msfazli76 <115671736+msfazli76@users.noreply.github.com>
2022-11-14 05:58:55 -0800

    Add files via upload

---
+++ letters.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+/* recognition of vowels */
+
+main() {
+	char letter;
+	cout<<"Plese enter a character:";
+	cin>>letter;
+	switch (letter){
+		case 'a':
+			cout<<"VOWEL";
+			break;
+		case 'o':
+			cout<<"VOWEL";
+			break;
+		case 'i':
+	    	cout<<"VOWEL";
+	    	break;
+	    case 'u':
+	    	cout<<"VOWEL";
+	    	break;
+	    case 'e':
+	    	cout<<"VOWEL";
+	    	break;
+	    case 'A':
+			cout<<"VOWEL";
+			break;
+		case 'O':
+			cout<<"VOWEL";
+			break;
+		case 'I':
+			cout<<"VOWEL";
+			break;
+		case 'E':
+			cout<<"VOWEL";
+			break;
+		case 'U':
+			cout<<"VOWEL";
+			break;
+			default:
+				cout<<"The entry is not vowel";
+				
+	}
+	
+}
